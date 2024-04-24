84ac9eaaf67228562ecb72e90f5d99fd02bf47c9
David Ko <davidsj.ko@gmail.com>
2024-04-24 16:57:21 -0400

    Refactor main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"html/template"
 	"log"
@@ -9,53 +8,26 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/velvetreactor/metrics/pkg/web"
 	"github.com/velvetreactor/metrics/pkg/ynabcollector"
 )
 
 func main() {
-	dbConn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Printf("unable to connect to database: %v", err)
-		os.Exit(1)
-	}
-	defer dbConn.Close(context.Background())
-
 	hourlyRegistry := prometheus.NewRegistry()
 
 	ynabCollector := &ynabcollector.YNABCollector{}
 	hourlyRegistry.MustRegister(ynabCollector)
 
-	r := chi.NewRouter()
+	web := web.New()
 
+	r := chi.NewRouter()
 	r.Route("/transactions", func(r chi.Router) {
 	})
 
 	r.Route("/notes", func(r chi.Router) {
-		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
-			err := r.ParseForm()
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-
-			commandTag, err := dbConn.Exec(
-				context.Background(),
-				"INSERT INTO notes (body) VALUES ($1)",
-				r.Form.Get("body"),
-			)
-
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			if commandTag.RowsAffected() != 1 {
-				log.Printf("Expected to affect 1 row, affected: %d", commandTag.RowsAffected())
-				return
-			}
-		})
+		r.Post("/", web.CreateNote)
 
 		r.Get("/new", func(w http.ResponseWriter, r *http.Request) {
 			file, err := os.ReadFile("views/add_note.html")
