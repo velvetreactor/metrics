f15b10e9bce24bd43682b1fd33ed56c87887359b
David Ko <davidsj.ko@gmail.com>
2024-04-23 21:06:48 -0400

    Working code change for creating notes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"html/template"
 	"log"
@@ -8,12 +9,20 @@ import (
 	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/jackc/pgx/v5"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/velvetreactor/metrics/pkg/ynabcollector"
 )
 
 func main() {
+	dbConn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Printf("unable to connect to database: %v", err)
+		os.Exit(1)
+	}
+	defer dbConn.Close(context.Background())
+
 	hourlyRegistry := prometheus.NewRegistry()
 
 	ynabCollector := &ynabcollector.YNABCollector{}
@@ -26,6 +35,26 @@ func main() {
 
 	r.Route("/notes", func(r chi.Router) {
 		r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			err := r.ParseForm()
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			commandTag, err := dbConn.Exec(
+				context.Background(),
+				"INSERT INTO notes (body) VALUES ($1)",
+				r.Form.Get("body"),
+			)
+
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			if commandTag.RowsAffected() != 1 {
+				log.Printf("Expected to affect 1 row, affected: %d", commandTag.RowsAffected())
+				return
+			}
 		})
 
 		r.Get("/new", func(w http.ResponseWriter, r *http.Request) {
